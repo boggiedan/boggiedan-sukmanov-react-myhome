d587f9003ba689f25c765748de5e76470d635fb6
boggie <sukmanov.b@gmail.com>
2020-11-29 23:29:40 +0100

    feat: vpn select feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [vpnList, setVpnList] = useState([])
+  const [selectedVpn, setSelectedVpn] = useState(null)
+  const [routerIp, setRouterIp] = useState('192.168.1.17')
+
+  const api = `http://${routerIp}:8000`
+
+  const fetchVpnList = () => {
+    setSelectedVpn(null)
+    fetch(`${api}/vpn/list`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+        .then((response) => response.json())
+        .then((response) => setVpnList(response?.vpns))
+        .catch((err) => alert('WTF: ' + err))
+  }
+
+  const sendSelectedVpn = () => {
+    fetch(`${api}/vpn/set`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ vpn: selectedVpn })
+    }).then((response) => {
+      if (!response.ok) alert(`${response.status}: ${response.statusText}`)
+      else alert(`${response.status}: ${response.statusText} (patience ça tourne)`)
+    })
+  }
+
+  useEffect(() => {
+    fetchVpnList()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <p>Usual Ips:</p>
+        <p>192.168.1.17</p>
+        <p>192.168.0.10</p>
+        <label>
+          Ou est mon routeur:
+          <input type="text" value={routerIp} onChange={(e) => setRouterIp(e.target.value)}/>
+        </label>
+      </div>
+      <div className="wrapper">
+        <div className="maxWidth">
+          <select value={selectedVpn} onChange={e => setSelectedVpn(e.target.value)}>
+            <option disabled selected value> -- il faut selectionner quelque chose -- </option>
+            {vpnList.map(({label, value}) => (
+                <option value={value}>{label}</option>
+            ))}
+          </select>
+          <button className="button" onClick={fetchVpnList}>Rafraîchir la liste</button>
+          <button className="button" onClick={() => selectedVpn && sendSelectedVpn()}>Envoyer</button>
+        </div>
+      </div>
     </div>
   );
 }
